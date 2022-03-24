864dc5583f1763f92e30befe2b5b4d5eea0ee784
Jakob <36604022+bakobjirner@users.noreply.github.com>
2022-03-24 18:25:45 +0100

    implement simple scrolling landscape

--- Assets/LandscapeGenerator.cs
+++ Assets/LandscapeGenerator.cs
@@ -15,14 +15,24 @@ public class LandscapeGenerator : MonoBehaviour
     //the resolution of the world
     public int worldResolution = 10;
 
+    //scale of noise
+    public float noiseScale = 1;
+    //zoom of noise
+    public float noiseZoom = 1;
+    //the speed the landscape is moving at
+    public float scrollSpeed = 10;
+    private float position;
+
     // Start is called before the first frame update
     void Start()
     {
         meshFilter = this.GetComponent<MeshFilter>();
         mesh = new Mesh();
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
         meshFilter.mesh = mesh;
 
         CreatePlane();
+        ApplyNoise();
         CreateTriangles();
         UpdateMesh();
     }
@@ -30,25 +40,25 @@ public class LandscapeGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ApplyNoise();
+        mesh.vertices = vertices;
+        mesh.RecalculateNormals();
     }
 
 
     //create a basic plane
     private void CreatePlane()
     {
-       
-        //by default the world is square
-       
-        vertices = new Vector3[worldDimension*worldResolution* worldDimension * worldResolution];
+
+        vertices = new Vector3[(worldResolution + 1)* (worldResolution + 1)];
         //set verticePositions
         int index = 0;
-        for(int x = 0; x< worldDimension * worldResolution; x++)
+        for(int x = 0; x<= worldResolution; x++)
         {
-            for(int z = 0; z < worldDimension * worldResolution; z++)
+            for(int z = 0; z <= worldResolution; z++)
             {
-                float gridscale = 1f / worldResolution;
-                vertices[index] = new Vector3(x*gridscale, 0, z*gridscale);
+                float gridscale = worldDimension / worldResolution;
+                vertices[index] = new Vector3(x*gridscale-0.5f*worldDimension, 0, z*gridscale - 0.5f * worldDimension);
                 index++;
                 
             }
@@ -59,11 +69,26 @@ public class LandscapeGenerator : MonoBehaviour
     {
         
 
-        triangles = new int[3];
-        triangles[0] = 0;
-        triangles[1] = worldDimension * worldResolution + 1;
-        triangles[2] = 1;
-        
+        //create one quad cosisting out of two triangles per loop
+        int quad = 0;
+        int tris = 0;
+        triangles = new int[worldResolution * worldResolution * 6];
+        for(int z = 0; z< worldResolution; z++)
+        {
+        for (int x = 0; x < worldResolution; x++)
+        {
+            triangles[0 + tris] = quad + 0;
+            triangles[1 + tris] = quad + 1;
+            triangles[2 + tris] = quad + worldResolution + 1;
+            triangles[3 + tris] = quad + 1;
+            triangles[4 + tris] = quad + worldResolution + 2;
+            triangles[5 + tris] = quad + worldResolution + 1;
+            quad++;
+            tris += 6;
+        }
+        //prevent creation of triangles between rows
+            quad++;
+        }
 
     }
 
@@ -75,11 +100,19 @@ public class LandscapeGenerator : MonoBehaviour
         mesh.RecalculateNormals();
     }
 
-    private void OnDrawGizmos()
+    private void ApplyNoise()
     {
+
+        position += scrollSpeed * Time.deltaTime;
+
         for(int i = 0; i< vertices.Length; i++)
         {
-            Gizmos.DrawSphere(vertices[i], .1f);
+            float x = vertices[i].x;
+            float z = vertices[i].z;
+            //add 100 to x-value since the perlin noisefunction seems to be symmetrical and there's a visible seam otherwise
+            float y = Mathf.PerlinNoise(x*noiseZoom + 100, z*noiseZoom+position)*noiseScale;
+            vertices[i] = new Vector3(x, y, z);
         }
     }
+
 }

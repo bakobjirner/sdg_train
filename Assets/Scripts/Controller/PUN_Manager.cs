8a5c5d69214860fdce8ec0ff56a8ebd65b255236
Robin Platz <robinplatz@gmail.com>
2022-03-24 22:29:22 +0100

    prototype pun playercontroller and manager

---
+++ Assets/Scripts/Controller/PUN_Manager.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PUN_Manager : MonoBehaviourPunCallbacks
+{
+    public static PUN_Manager Instance;
+
+    public GameObject Player;
+    private GameObject RespawnLocation;
+
+    GameObject PlayerInstance;
+
+    void Start() {
+        Instance = this;
+        RespawnLocation = GameObject.FindGameObjectWithTag("RespawnLocation");
+        if(PlayerController.LocalPlayerInstance == null) {
+            this.PlayerInstance = PhotonNetwork.Instantiate(this.Player.name, this.RespawnLocation.transform.position + Vector3.up * 5, Quaternion.identity);
+        }
+    }
+
+    public void RespawnPlayer(String team) {
+        RespawnLocation = GameObject.FindGameObjectWithTag("RespawnLocation");
+        PhotonNetwork.Destroy(PlayerController.LocalPlayerInstance);
+        this.PlayerInstance = PhotonNetwork.Instantiate(this.Player.name, this.RespawnLocation.transform.position + Vector3.up * 5, Quaternion.identity);
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    void LoadArena()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+        }
+        Debug.LogFormat("PhotonNetwork : PlayerCount : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+        PhotonNetwork.LoadLevel("MultiplayerSample");
+    }
+
+    public override void OnPlayerEnteredRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+        }
+    }
+}

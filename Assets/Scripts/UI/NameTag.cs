f2b2c1f8767383dc4c33828619b9f08bbcf354c7
nimartin99 <nic0m4rtin@gmail.com>
2022-05-05 22:29:09 +0200

    Fixed Nametags

--- Assets/Scripts/UI/NameTag.cs
+++ Assets/Scripts/UI/NameTag.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class NameTag : MonoBehaviourPun
 {
-    public string getPlayerName;
-    public TextMesh nameTag;
+    public TextMeshPro nameTag;
     void Start()
     {
         if(!photonView.IsMine)
         {
-            nameTag = gameObject.GetComponent<TextMesh>();
+            nameTag = gameObject.GetComponent<TextMeshPro>();
             nameTag.text = photonView.Owner.NickName;
         } else
         {
@@ -22,5 +22,8 @@ public class NameTag : MonoBehaviourPun
     private void Update()
     {
         gameObject.transform.LookAt(PUN_Manager.Instance.PlayerInstance.gameObject.transform);
+        Vector3 rotation = Quaternion.LookRotation(PUN_Manager.Instance.PlayerInstance.gameObject.transform.position).eulerAngles;
+        rotation.x = 0f;
+        transform.rotation = Quaternion.Euler(rotation);
     }
 }
